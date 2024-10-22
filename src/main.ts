acda88f29475c57e1915c8e8ce77471e0abe6545
firekicks <114191519+firekicks@users.noreply.github.com>
2024-10-22 11:03:09 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,16 @@ const APP_NAME = "Hello, this is Atri!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+app.innerHTML = ""; 
+
+const appTitle = document.createElement('h1');
+appTitle.textContent = APP_NAME;
+
+const drawingCanvas = document.createElement('canvas');
+drawingCanvas.width = 256;
+drawingCanvas.height = 256;
+drawingCanvas.id = "myCanvas"; 
+
+app.appendChild(appTitle);
+app.appendChild(drawingCanvas);
